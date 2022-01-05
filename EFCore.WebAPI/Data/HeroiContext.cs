dbdcc8f500af67fea0c18e4057f0aa64aa1bef0d
Gabriel Borges <gbdesigns13@gmail.com>
2022-01-05 14:34:01 -0300

    initial

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,30 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=12345;Persist Security Info=True;User ID=sa;Initial Catalog=Hero App;Data Source=DESKTOP-Q4QASJS\\SQLCUSTOM");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroId });
+            });
+        }
+    }
+}
---
+++ EFCore.WebAPI/Models/Heroi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public IdentidadeSecreta Identidade { get; set; }
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+    }
+}
